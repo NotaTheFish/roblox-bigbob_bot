bc99f00e431de00be3bd45a52a709bfbca7fa6e1
NotaTheFish <edogasha@gmail.com>
2025-11-13 23:33:42 +0100

    Migrations

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,8 @@ import os
 from logging.config import fileConfig
 
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+from sqlalchemy.engine import make_url
 
 # --- добавляем путь проекта (чтобы можно было импортировать backend, bot, db) ---
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -19,11 +21,12 @@ from bot.db import Base
 target_metadata = Base.metadata
 
 # --- выставляем URL для Alembic ---
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
+database_url = os.getenv("DATABASE_URL")
+if not database_url:
     raise RuntimeError("DATABASE_URL not found")
 
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+sync_database_url = make_url(database_url).set(drivername="postgresql+psycopg2")
+config.set_main_option("sqlalchemy.url", str(sync_database_url))
 
 
 # -----------------------------------------------------------------------
@@ -31,7 +34,7 @@ config.set_main_option("sqlalchemy.url", DATABASE_URL)
 # -----------------------------------------------------------------------
 def run_migrations_offline() -> None:
     context.configure(
-        url=DATABASE_URL,
+        url=str(sync_database_url),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -44,11 +47,13 @@ def run_migrations_offline() -> None:
 # -----------------------------------------------------------------------
 # ONLINE (нормальные миграции)
 # -----------------------------------------------------------------------
-from sqlalchemy import create_engine
-
-
 def run_migrations_online() -> None:
-    connectable = create_engine(DATABASE_URL)
+    configuration = config.get_section(config.config_ini_section, {})
+    connectable = engine_from_config(
+        configuration,
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
